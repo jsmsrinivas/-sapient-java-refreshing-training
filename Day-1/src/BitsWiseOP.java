9f91f9df36b5f15d13d08c34daf08c5af64b341f
Mogunda Srinivas <mogsrini@publicisgroupe.net>
2022-04-08 16:56:41 +0530

    intial commit

---
+++ Day-1/src/BitsWiseOP.java
@@ -0,0 +1,15 @@
+public class BitsWiseOP
+{ 
+	public static void main(String args[]) 
+{ 
+		byte a=10,b=11;
+
+System.out.println ("~a="+(~a));
+System.out.println ("a & b="+(a&b));
+System.out.println ("a | b="+(a|b));
+System.out.println ("a ^ b="+(a^b));
+System.out.println ("a<<2="+(a<<2));
+System.out.println ("a>>2="+(a>>2));
+System.out.println ("a>>>2="+(a>>>2));
+}
+}
