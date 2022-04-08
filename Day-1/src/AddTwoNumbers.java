4ace2d4cd6b67d1cf8bc6a0039c60917cdc67987
Mogunda Srinivas <mogsrini@publicisgroupe.net>
2022-04-08 16:52:45 +0530

    inital commit

---
+++ Day-1/src/AddTwoNumbers.java
@@ -0,0 +1,12 @@
+public class AddTwoNumbers
+{ 
+public static void main(String args[])
+{ 
+		int a=10, b=20;
+System.out.println("Addition of two numbers is : " + (a+b));
+System.out.println("Subtraction of two numbers is : " + (a-b));
+System.out.println("Multiplication of two numbers is : " + (a*b));
+System.out.println("Quotient after division is : " + (a/b));
+System.out.println("Remainder after division is : " +(a%b));
+}
+}
