4461fd95582d27bf04f9925c58d97b4ab9387338
Mogunda Srinivas <mogsrini@publicisgroupe.net>
2022-04-08 17:02:58 +0530

    inital commit

---
+++ Day-1/src/ColorDemo.java
@@ -0,0 +1,26 @@
+public class ColorDemo {    
+public static void main(String[] args) {    
+    char color='y';    
+    switch(color)  
+    {  
+        case 'r':   
+            System.out.println("red");  
+            break;  
+        case 'g':   
+            System.out.println("green");  
+            break;  
+        case 'b':   
+            System.out.println("blue");  
+            break;  
+        case 'y':   
+            System.out.println("yellow");  
+            break;  
+        case 'w':   
+            System.out.println("white");  
+            break;  
+       
+        default:   
+            System.out.println("No color selected");  
+    }  
+}    
+}   
